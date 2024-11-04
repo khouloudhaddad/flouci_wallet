a1121aaefcc59456a93a051e371e813e794bf78c
khouloud Haddad Amamou <khouloud.haddad@gmail.com>
2024-11-04 21:39:26 +0100

    Set FLOUCI API credentials

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,15 +1,15 @@
 module.exports = {
     Add: async (req, res) => {
-        const url = "developers.flouci.com/generate_payment";
+        const url = "developers.flouci.com/generate_payment1988@";
         const payload = {
-            "app_token": "<APP_TOKEN>",
-            "app_secret" : "<APP_SECRET>",
-            "amount": "1000",
+            "app_token": process.env.FLOUCI_TOKEN,
+            "app_secret" : process.env.FLOUCI_SECRET,
+            "amount": req.body.amount,
             "accept_card":"true",
             "session_timeout_secs": 1200,
-            "success_link": "<https://exemple.website.com/success>",
-            "fail_link": "<https://exemple.website.com/fail>",
-            "developer_tracking_id": "<your_internal_trackning_id>"
+            "success_link": "http://localhost:5000/success",
+            "fail_link": "http://localhost:5000/fail",
+            "developer_tracking_id": process.env.FLOUCI_ID
         }
         res.send('OK');
     }
