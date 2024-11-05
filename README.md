9706ec33c55a7073f0d0761c6ccda7f6704e7ea7
khouloud Haddad Amamou <khouloud.haddad@gmail.com>
2024-11-05 01:03:25 +0100

    Verify payment status

--- README.md
+++ README.md
@@ -8,3 +8,13 @@ Setup Payments with FLOUCI wallet, ReactJs And NodeJs
 - npm i nodemon -D
 - npm i dotenv 
 - npm i axios
+
+## Run server and test payment
+- npm run server
+- Test API (http://localhost:5000/api/payment) on Postman or thunderclient vs code extension,...
+<br/><br/>
+<img src="https://i.ibb.co/zss7bnx/thunderclient-flouci.png" width="100%" alt="Thunder client">
+<br/><br/>
+- [FLOUCI API Documentation](https://flouci.stoplight.io/docs/flouci-payment-apis/6c9b5ad7358c9-fr-flouci-payment-api)
+
+
--- controllers/payment.js
+++ controllers/payment.js
@@ -1,23 +1,41 @@
-const axios = require("axios")
+const axios = require("axios");
 
 module.exports = {
-    Add: async (req, res) => {
-        const url = "https://developers.flouci.com/api/generate_payment";
-        const payload = {
-            "app_token": process.env.FLOUCI_TOKEN,
-            "app_secret" : process.env.FLOUCI_SECRET,
-            "amount": req.body.amount,
-            "accept_card":"true",
-            "session_timeout_secs": 1200,
-            "success_link": "http://localhost:5000/success",
-            "fail_link": "http://localhost:5000/fail",
-            "developer_tracking_id": process.env.FLOUCI_ID
-        }
-        
-        await axios.post(url, payload)
-        .then((result) => {
-            res.send(result.data)
-        })
-        .catch(err => console.log(err));
-    }
-}
+  Add: async (req, res) => {
+    const url = "https://developers.flouci.com/api/generate_payment";
+    const payload = {
+      app_token: process.env.FLOUCI_TOKEN,
+      app_secret: process.env.FLOUCI_SECRET,
+      amount: req.body.amount,
+      accept_card: "true",
+      session_timeout_secs: 1200,
+      success_link: "http://localhost:5000/success",
+      fail_link: "http://localhost:5000/fail",
+      developer_tracking_id: process.env.FLOUCI_ID,
+    };
+
+    await axios
+      .post(url, payload)
+      .then((result) => {
+        res.send(result.data);
+      })
+      .catch((err) => console.log(err));
+  },
+  Verify: async (req, res) => {
+    const payment_id = req.params.id;
+    const url = `https://developers.flouci.com/api/verify_payment/${payment_id}`;
+
+    await axios
+      .get(url, {
+        headers: {
+          "Content-Type": "application/json",
+          apppublic: process.env.FLOUCI_TOKEN,
+          appsecret: process.env.FLOUCI_SECRET,
+        },
+      })
+      .then((result) => {
+        res.send(result.data);
+      })
+      .catch((err) => console.log(err));
+  },
+};
