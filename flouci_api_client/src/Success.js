f871e721f59792d882f918f3e7c5c08526f6d99d
khouloud Haddad Amamou <khouloud.haddad@gmail.com>
2024-11-05 16:22:02 +0100

    Finalize payment step & payment verification

--- flouci_api_client/src/Success.js
+++ flouci_api_client/src/Success.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
-function success() {
+function Success() {
+  const [searchParams] = useSearchParams();
+  const [output, setOutput] = useState("");
+  useEffect(() => {
+    axios
+      .get(`/api/payment/${searchParams.get("payment_id")}`)
+      .then((res) => {
+        setOutput(res.data.result.status);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
-    <div className='p-4'>
-      <div role="alert" className="alert alert-success">
-        <span>Payment success.</span>
-      </div>
-    </div>
-  )
+    <React.Fragment>
+      {output === "SUCCESS" && (
+        <div className="p-4">
+          <div role="alert" className="alert alert-success">
+            <span>Payment success.</span>
+          </div>
+        </div>
+      )}
+    </React.Fragment>
+  );
 }
 
-export default success
+export default Success;
